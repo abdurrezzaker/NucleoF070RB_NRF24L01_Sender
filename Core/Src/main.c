d00b97609d3907019c8cf59bd082e66d43937b75
abdurrezzaker <abdurezak.er@gmail.com>
2024-12-28 14:10:46 +0300

    Second commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -183,7 +183,7 @@ static void MX_SPI1_Init(void)
   hspi1.Instance = SPI1;
   hspi1.Init.Mode = SPI_MODE_MASTER;
   hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_4BIT;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
@@ -197,9 +197,6 @@ static void MX_SPI1_Init(void)
   if (HAL_SPI_Init(&hspi1) != HAL_OK)
   {
     Error_Handler();
-    printf("sikinti var.");
-  }else{
-	  printf("sikinti var.");
   }
   /* USER CODE BEGIN SPI1_Init 2 */
 
